f229a2d42c97eadfad0e6658ea888e58635580fe
Paul Giacchetto <giapaul_91@yahoo.com>
2025-02-26 20:05:02 -0500

    Bullets now kill the asteroid in 3 hits. Asteroid blinks to confirm a hit.

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @export var move_speed = 120
+@export var damage_points = 1
 
 func _physics_process(delta: float) -> void:
 	#So far, simple movement upwards. Maybe different bullets have different effects
@@ -9,3 +10,9 @@ func _physics_process(delta: float) -> void:
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	# Destroy bullet when it leaves the screen (no longer useful)
 	queue_free()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Shootable"):
+		area.take_damage(damage_points)
+		queue_free()
